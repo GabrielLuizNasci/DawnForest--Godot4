68b61648182caffbce72a3474878c53ca8518f00
Gabriel Luiz <gabriellnsilva2004@gmail.com>
2024-06-16 21:13:26 -0300

    Correção de código e adição do comando de salto e gravidade.

--- scripts/player/personagem.gd
+++ scripts/player/personagem.gd
@@ -3,12 +3,21 @@ extends CharacterBody2D
 class_name Player
 
 var direcao_input = Vector2(0,0)
-@export var SPEED = 100.0
-const JUMP_VELOCITY = -400.0
+
+# Variáveis relacionadas a velocidade e movimentação
+@export var speed = 100.0
+var ultima_direcao_horizontal = 1
+
+# Variáveis relacionadas a salto
+@export var jump_speed = -400.0
+@export var player_gravity = 1000
+var jump_cont: int = 0
+var landing: bool = false
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 # var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var gravity = 1000
+# var gravity = 1000
 
 func _process(_delta):
 	direcao_input = Input.get_vector("esquerda","direita","salto","agachamento")
@@ -20,36 +29,44 @@ func _process(_delta):
 		if (direcao_input.x == 1):
 			$AnimatedSprite2D.flip_h = false
 			$AnimatedSprite2D.play("corrida")
+			ultima_direcao_horizontal = 1
 		elif (direcao_input.x == -1):
 			$AnimatedSprite2D.flip_h = true
 			$AnimatedSprite2D.play("corrida")
+			ultima_direcao_horizontal = -1
 	else:
 		$AnimatedSprite2D.play("idle")
+		$AnimatedSprite2D.flip_h = ultima_direcao_horizontal == -1
 
 func _physics_process(delta):
-	if(direcao_input.length() > 0):
-		velocity = direcao_input * SPEED
-		#position.x = clamp(position.x, 0, get_viewport_rect().size.x)
-		#position.y = clamp(position.y, 0, get_viewport_rect().size.y)
-	else:
-		# Aplica desaceleração quando não há input
-		velocity.x = lerp(velocity.x, 0.0, 0.1)
-		velocity.y = lerp(0.0, velocity.y, 0.2)
-	print(direcao_input)
- 
+	controle_movimento_horizontal()
+	controle_movimento_vertical()
+	gravity(delta)
 	# Handle jump.
-	if Input.is_action_just_pressed("salto") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	# var movimento_horizontal: float = Input.get_action_strength("direita") - Input.get_action_strength("esquerda")
-	#if Input.is_action_just_pressed("esquerda") or Input.is_action_just_pressed("direita"):
-
-		
-	
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity.y += gravity * delta
+	# if Input.is_action_just_pressed("salto") and is_on_floor():
+		# velocity.y = JUMP_VELOCITY
 	
 	move_and_slide()
+
+func controle_movimento_horizontal() -> void:
+	var input_direction: float = Input.get_action_strength("direita") - Input.get_action_strength("esquerda")
+	
+	velocity.x = input_direction * speed
+	
+	if(is_zero_approx(input_direction)):
+		# Aplica desaceleração quando não há input
+		velocity.x = lerp(velocity.x, 0.0, 0.1)
+
+func controle_movimento_vertical() -> void:
+	if is_on_floor():
+		jump_cont = 0
+	
+	if Input.is_action_just_pressed("salto") and jump_cont < 2:
+		jump_cont += 1
+		velocity.y = jump_speed
+
+func gravity(delta: float) -> void:
+	# Add the gravity.
+	velocity.y += player_gravity * delta
+	if velocity.y >= player_gravity:
+		velocity.y = player_gravity
