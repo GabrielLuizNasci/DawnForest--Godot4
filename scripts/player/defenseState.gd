1354bf24c7debffcf2ccd73f9061658d51db71af
Gabriel Luiz <gabriellnsilva2004@gmail.com>
2024-09-27 12:31:41 -0300

    Estado de Defesa implementado por completo

---
+++ scripts/player/defenseState.gd
@@ -0,0 +1,24 @@
+extends State
+
+class_name DefenseState
+
+@export var return_state: State
+@export var animacao_retorno: String = "mover"
+@export var animacao_defesa: String = "defesa"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func state_input(event: InputEvent):
+	if(event.is_action_pressed("defesa")):
+		playback.travel(animacao_defesa)
+	elif(event.is_action_released("defesa")):
+		next_state = return_state
+		playback.travel(animacao_retorno)
--- scripts/player/groundState.gd
+++ scripts/player/groundState.gd
@@ -7,10 +7,12 @@ class_name GroundState
 #Estados
 @export var air_state: State
 @export var attack_state: State
+@export var defense_state: State
 
 #Animações
 @export var animacao_salto: String = "salto"
 @export var animacao_ataque: String = "ataque1"
+@export var animacao_defesa: String = "defesa"
 
 var jump_cont: int = 0
 
@@ -23,6 +25,8 @@ func state_input(event: InputEvent):
 		salto()
 	if(event.is_action_pressed("ataque")):
 		ataque()
+	if(event.is_action_pressed("defesa")):
+		defesa()
 
 func salto():
 	character.velocity.y = jump_speed
@@ -32,3 +36,7 @@ func salto():
 func ataque():
 	next_state = attack_state
 	playback.travel(animacao_ataque)
+
+func defesa():
+	next_state = defense_state
+	playback.travel(animacao_defesa)
